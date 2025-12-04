df0307486eeac03f95cde3bac701d63cd01cf981
saidm <smartelod@unicartagena.edu.co>
2025-12-04 00:11:05 -0500

    Proyecto inicial spring boot

---
+++ src/main/java/co/com/udec/IngSoftTCC/IngSoftTccApplication.java
@@ -0,0 +1,13 @@
+package co.com.udec.IngSoftTCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngSoftTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngSoftTccApplication.class, args);
+	}
+
+}
